27667503924e7077cc83341974d1735514c66ee4
DariaGoncharevskaia <d12598@mail.ru>
2021-09-17 13:53:10 +0300

    HM2

---
+++ homework2/2_files.py
@@ -0,0 +1,26 @@
+  
+"""
+Домашнее задание №2
+Работа с файлами
+1. Скачайте файл по ссылке https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0
+2. Прочитайте содержимое файла в перменную, подсчитайте длинну получившейся строки
+3. Подсчитайте количество слов в тексте
+4. Замените точки в тексте на восклицательные знаки
+5. Сохраните результат в файл referat2.txt
+"""
+
+def main():
+    """
+    Эта функция вызывается автоматически при запуске скрипта в консоли
+    В ней надо заменить pass на ваш код
+    """
+    with open("referat.txt", "r", encoding='utf-8') as referat:
+        text = referat.read()
+        print(len(text))
+        print(len(text.split()))
+        with open("referat2.txt", "w", encoding='utf-8') as referat2:
+            referat2.write(text.replace(".","!"))
+
+
+if __name__ == "__main__":
+    main()
